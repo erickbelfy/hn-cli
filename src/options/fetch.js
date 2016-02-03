a3ebe68cbd17eeff9ae81025bf73f3191e49881d
Erick Belfort <erick.belf@gmail.com>
2016-02-03 14:40:37 -0200

    investigating proxy reasons for #4

--- src/options/fetch.js
+++ src/options/fetch.js
@@ -1,7 +1,13 @@
+const tunnel = require('tunnel');
 module.exports = {
   json: true,
   timeout: 8000,
   headers: {
     'user-agent': 'https://github.com/rafaelrinaldi/hn-cli'
-  }
+  },
+  agent: tunnel.httpsOverHttp({
+    proxy: {
+      host: 'localhost'
+    }
+  })
 };
